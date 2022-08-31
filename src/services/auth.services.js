ebe2217f1852b59dbe1cda1803bca2af3ea073db
Yoni Ifrah <yoniifrah@Yonis-MBP.Dlink>
2022-08-31 18:49:17 +0300

    added firebase

---
+++ src/services/auth.services.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+
+
+
+const app = initializeApp(firebaseConfig);
+
+
+const auth = getAuth();
